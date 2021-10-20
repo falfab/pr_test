1058de9421d2eb3d37a42a10abd2a52a317b15de
Fabio Falezza <fabio.falezza@franka.de>
2021-10-20 15:12:43 +0200

    improved after review

--- src/feature.cpp
+++ src/feature.cpp
@@ -4,6 +4,5 @@ int main()
 {
     std::cout << "Hello world is NOT *I want to keep the internal* a great feature" << std::endl;
     int return_code = 1;
-    return_code++;
-    return return_code++;
+    return return_code--;
 }
